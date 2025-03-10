ce3a7873a6647f6e87cbd22f2c99ca04d55f6d62
Nour Saif Yassine <nour.saif.yassine@gmail.com>
2025-03-10 13:23:52 +0100

    add: Stop-Process for prgramming

--- BreakLocker.ps1
+++ BreakLocker.ps1
@@ -1,5 +1,7 @@
-$SchoolDays = "monday", "tuesday", "wednesday", "Thursday", "Friday" # Adjust the days if necessary
-$SchoolPauzes = "11:00", "12:30", "14:30", "10:13" # Adjust the time if necessary
+# Maybe need to change the language to adjust the days if necessary
+$DayBreaks = "monday", "tuesday", "wednesday", "thursday", "friday"
+$TimeBreaks = "11:00", "12:30", "14:30", "13:18" # Adjust the time if necessary
+$ProgramingLanguages = "node", "php", "python" # Adjust the programming languages if necessary
 $wshell = New-Object -ComObject Wscript.Shell
 $lastTriggeredTime = ""
 
@@ -8,12 +10,34 @@ while ($true) {
     $GetTime = Get-Date -Format "HH:mm"
     $GetDay = Get-Date -Format "dddd"
 
-    if ($SchoolDays -contains $GetDay) {
-        if ($SchoolPauzes -contains $GetTime -and $lastTriggeredTime -ne $GetTime) {
-            Write-Host "Break time on $GetTime"
-            $wshell.Popup("Break Time! Time to close your Windows device", 0, "Break Time", 0x0)
-            rundll32.exe user32.dll,LockWorkStation
-            $lastTriggeredTime = $GetTime
+    if ($DayBreaks -contains $GetDay) {
+        Write-Host "Test dag"
+        if ($TimeBreaks -contains $GetTime -and $lastTriggeredTime -ne $GetTime) {
+            if (Get-Process | Where-Object { $_.ProcessName -in $ProgramingLanguages }) {
+                Write-Host "Break time on $GetTime"
+                $StopInput = Read-Host -Prompt "Do you want to stop coding(stop the process type yes/no)?"
+                if ($StopInput -eq "yes") {
+                    Write-Host "Stopping the process $GetTime"
+                    foreach ($language in $ProgramingLanguages) { 
+                        Stop-Process -Name $language -Force -ErrorAction SilentlyContinue 
+                    }
+                    $lastTriggeredTime = $GetTime
+                } elseif ($StopInput -eq "no") {
+                    Write-Host "Continue coding"
+                    Start-Sleep -Seconds 60
+                    $lastTriggeredTime = $GetTime
+                } else {
+                    Write-Host "Invalid input"
+                }
+                $wshell.Popup("Break Time! Time to stop coding on your Windows device", 0, "Break Time", 0x0)
+                rundll32.exe user32.dll,LockWorkStation
+                $lastTriggeredTime = $GetTime
+            } else {
+                Write-Host "Break time on $GetTime"
+                $wshell.Popup("Break Time! Time to close your Windows device", 0, "Break Time", 0x0)
+                rundll32.exe user32.dll,LockWorkStation
+                $lastTriggeredTime = $GetTime
+            }
         }
     }
     
